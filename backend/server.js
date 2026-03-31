fb1a69d846903be644054bd492b0b76256fda5f7
alex@panther <170464688+thegamebambino@users.noreply.github.com>
2026-03-31 16:10:12 -0500

    Fixed db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
+import dotenv from 'dotenv'; import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 
 dotenv.config();
@@ -13,6 +12,6 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log('Server is running on port:', PORT);
+  connectDB();
+  console.log('Server is running on port:', PORT);
 });
